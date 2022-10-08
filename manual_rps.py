fc72c12a9b0131f741306d8cca72776df78e224a
Amalaseeli <amalremi07@gmail.com>
2022-10-08 17:57:03 +0100

    updated readme.md file

--- manual_rps.py
+++ manual_rps.py
@@ -11,14 +11,15 @@ def get_user_choice():
     user_choice=input("Enter your coice:")
     return user_choice
 
-computer_choice=get_computer_choice()
-user_choice=get_user_choice()
-print(computer_choice)
-if (computer_choice=="rock" and user_choice=="scissors") or (computer_choice=="paper" and user_choice=="rock") or (computer_choice=="scissors" and user_choice=="paper"):
-    print("Computer wins") 
-elif (computer_choice=="rock" and user_choice=="paper") or (computer_choice=="paper" and user_choice=="scissors") or (computer_choice=="scissors" and user_choice=="rock"):
-    print("User wins") 
-else:
-    print("Draw game")
+def play():
+    computer_choice=get_computer_choice()
+    user_choice=get_user_choice()
+    print(computer_choice)
+    if (computer_choice=="rock" and user_choice=="scissors") or (computer_choice=="paper" and user_choice=="rock") or (computer_choice=="scissors" and user_choice=="paper"):
+        print("Computer wins") 
+    elif (computer_choice=="rock" and user_choice=="paper") or (computer_choice=="paper" and user_choice=="scissors") or (computer_choice=="scissors" and user_choice=="rock"):
+        print("User wins") 
+    else:
+        print("Draw game")
 
-    
+play()   
